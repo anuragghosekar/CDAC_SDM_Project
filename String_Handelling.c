0dcf9c0c47cc5d17779b5dc9907d4a2c07f0eabf
Anurag Ghosekar <70033683+anuragghosekar@users.noreply.github.com>
2023-09-12 03:35:37 -0700

    Update String_Handelling.c

--- String_Handelling.c
+++ String_Handelling.c
@@ -87,28 +87,4 @@ void ustrcat(char* p, char* q)
 
 
 
-#include <stdio.h>
-#include <string.h>
 
-//void check_palindrome(char *a);
-//
-//int main()
-//{
-//	char a[]={"Anurag"};
-//	char *b=&a[];
-//	int i,j;
-//	int l=strlen(a);
-//	int flag=0;
-//	
-//	for(i=0,j=l-1;j>i;i++,j--)
-//	{
-//		char ch=a[i];
-//		a[i]=a[j];
-//		a[j]=ch;
-//		
-//	}
-//	printf("%s\n",b);
-//	printf("%s\n",a);
-//	
-//	
-//}
